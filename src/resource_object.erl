cca30142796145c7dea4e2ad47740de3c474c5d6
Wilson Júnior <wilsonpjunior@gmail.com>
2015-04-12 20:26:12 -0300

    resourceObject can be converted to bson
    Signed-off-by: Wilson Júnior <wilsonpjunior@gmail.com>

--- src/resource_object.erl
+++ src/resource_object.erl
@@ -1,5 +1,6 @@
 -module(resource_object).
--export([from_bson/1, from_bson/2]).
+-export([from_bson/1, from_bson/2,
+         to_bson/1, to_bson/2]).
 
 from_bson(BsonDocument) ->
     from_bson(BsonDocument, id).
@@ -8,6 +9,14 @@ from_bson(BsonDocument, PrimaryKeyName) ->
     {[{'_id', PrimaryKeyValue} | RemaingValues]} = to_proplist(BsonDocument),
     {[{PrimaryKeyName, PrimaryKeyValue} | RemaingValues]}.
 
+to_bson(Document) ->
+    to_bson(Document, id).
+
+to_bson(Document, PrimaryKeyName) ->
+    {AttrList} = Document,
+    PrimaryKeyValue = proplists:get_value(PrimaryKeyName, AttrList),
+
+    recursive_convert_to_bson_document({[{'_id', PrimaryKeyValue} | proplists:delete(PrimaryKeyName, AttrList)]}).
 
 to_proplist([]) ->
 	[];
@@ -26,4 +35,10 @@ recursive_convert(Value) when erlang:is_tuple(Value) ->
     to_proplist(Value);
 
 recursive_convert(Value) ->
+    Value.
+
+recursive_convert_to_bson_document(Value = {PropList}) when erlang:is_list(PropList)->
+    bson:document([{Key, recursive_convert_to_bson_document(Value)} || {Key, Value} <- PropList]);
+
+recursive_convert_to_bson_document(Value) ->
     Value.
--- test/test_resource_object.erl
+++ test/test_resource_object.erl
@@ -1,24 +1,46 @@
 -module(test_resource_object).
 -include_lib("eunit/include/eunit.hrl").
 
-one_depth_test() ->
+from_bson_one_depth_test() ->
     ?assertEqual({[{id, "123"}, {name, "wilson"}]},
                  resource_object:from_bson({'_id', "123", name, "wilson"})).
 
-two_depth_test() ->
+from_bson_two_depth_test() ->
     InputEmbeddedField = {field1, "321", field2, 3},
     ExpectedEmbeddedField = {[{field1, "321"}, {field2, 3}]},
     ?assertEqual({[{id, "123"}, {name, "wilson"}, {second, ExpectedEmbeddedField}]},
                  resource_object:from_bson({'_id', "123", name, "wilson",
                                             second, InputEmbeddedField})).
 
-one_depth_with_custom_primary_key_test() ->
+from_bson_one_depth_with_custom_primary_key_test() ->
     ?assertEqual({[{slug, "123"}, {name, "wilson"}]},
                  resource_object:from_bson({'_id', "123", name, "wilson"}, slug)).
 
-two_depth_with_custom_primary_key_test() ->
+from_bson_two_depth_with_custom_primary_key_test() ->
     InputEmbeddedField = {field1, "321", field2, 3},
     ExpectedEmbeddedField = {[{field1, "321"}, {field2, 3}]},
     ?assertEqual({[{customId, "123"}, {name, "wilson"}, {second, ExpectedEmbeddedField}]},
                  resource_object:from_bson({'_id', "123", name, "wilson",
                                             second, InputEmbeddedField}, customId)).
+
+to_bson_one_depth_test() ->
+    ?assertEqual({'_id', "123", name, "wilson"},
+                 resource_object:to_bson({[{id, "123"}, {name, "wilson"}]})).
+
+to_bson_two_depth_test() ->
+    InputEmbeddedField = {[{field1, "321"}, {field2, 3}]},
+    ExpectedEmbeddedField = {field1, "321", field2, 3},
+
+    ?assertEqual({'_id', "123", name, "wilson", second, ExpectedEmbeddedField},
+                 resource_object:to_bson({[{id, "123"}, {name, "wilson"}, {second, InputEmbeddedField}]})).
+
+to_bson_one_depth_with_custom_primary_key_test() ->
+    ?assertEqual({'_id', "123", name, "wilson"},
+                 resource_object:to_bson({[{slug, "123"}, {name, "wilson"}]}, slug)).
+
+to_bson_two_depth_with_custom_primary_key_test() ->
+    InputEmbeddedField = {[{field1, "321"}, {field2, 3}]},
+    ExpectedEmbeddedField = {field1, "321", field2, 3},
+
+    ?assertEqual({'_id', "123", name, "wilson", second, ExpectedEmbeddedField},
+                 resource_object:to_bson({[{customId, "123"}, {name, "wilson"}, {second, InputEmbeddedField}]}, customId)).
